ea8e6ff74a5695847424c86b4db2754717242937
AdamNebrat123 <adam.nebrat@gmail.com>
2025-09-26 12:14:26 +0300

    Now the server listens on http://0.0.0.0:5000, so i connect to him from another pc in my LAN.

--- Server/Src/Core/WebSocketServer.cs
+++ Server/Src/Core/WebSocketServer.cs
@@ -16,6 +16,9 @@ public class WebSocketServer
         
 
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
         var app = builder.Build();
 
         app.UseWebSockets();
